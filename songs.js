ba390d6b2d1c1376ac548a347059a9362c36df66
Henry Allen-Tilford <htilford@gmail.com>
2012-07-03 15:28:22 -0700

    Update master

---
+++ songs.js
@@ -0,0 +1,110 @@
+var request = require('request');
+
+function songs (parameters, cb) {
+  if (typeof parameters === 'function') {
+    return songsBy({}, parameters);
+  }
+  if (parameters.by || parameters.prefix) {
+    return songsBy(parameters, cb);
+  }
+  if (parameters.id) {
+    return songById(parameters.id, cb);
+  }
+  if (parameters.ids) {
+    return songsByIds(parameters.ids, cb);
+  }
+  if (parameters.query) {
+    return songsBySearch(parameters, cb);
+  }
+  if (parameters === 'languages') {
+    return getLanguages(cb);
+  }
+  if (parameters === 'stats') {
+    return getStats(cb);
+  }
+  return songsBy({}, cb);
+}
+
+function songsBy (parameters, cb) {
+  return request({
+    url: apiUrl + 'songs.json',
+    qs: parameters,
+    json: true
+  }, function (error, response, body) {
+    if (error) {
+      return cb(error);
+    }
+    return cb(null, body);
+  });
+}
+function songById (id, cb) {
+  return request({
+    url: apiUrl + 'songs/' + id + '.json',
+    json: true
+  }, function (error, response, body) {
+    if (error) {
+      return cb(error);
+    }
+    return cb(null, body);
+  });
+}
+function songsByIds (ids, cb) {
+  var songs = [];
+  return ids.forEach(function (id) {
+    return songById(id, gotSong);
+  });
+  function gotSong (error, song) {
+    if (cb === null) {
+      return false;
+    }
+    if (error) {
+      cb(error);
+      cb = null;
+      return false;
+    }
+    songs.push(song);
+    if (songs.length === ids.length) {
+      return cb(null, songs);
+    }
+  }
+}
+function songsBySearch (parameters, cb) {
+  return request({
+    url: apiUrl + '/songs/search.json',
+    qs: parameters,
+    json: true
+  }, function (error, response, body) {
+    if (error) {
+      return cb(error);
+    }
+    return cb(null, body);
+  });
+}
+function getLanguages (cb) {
+  return request({
+    url: apiUrl + 'songs/languages.json',
+    json: true
+  }, function (error, response, body) {
+    if (error) {
+      return cb(error);
+    }
+    return cb(null, body);
+  });
+}
+function getStats (cb) {
+  return request({
+    url: apiUrl + 'songs/stats.json',
+    json: true
+  }, function(error, response, body) {
+    if (error) {
+      return cb(error);
+    }
+    return cb(null, body);
+  });
+}
+
+module.exports = songs;
+
+if (!module.parent) {
+  songs(console.log)
+}
