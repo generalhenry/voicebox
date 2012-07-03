d1b9584c370fc357083c5c86eea817970cde6425
Henry Allen-Tilford <htilford@gmail.com>
2012-07-03 15:47:06 -0700

    Update master

--- vb.js
+++ vb.js
@@ -1,37 +1,4 @@
-var express = require('express');
-var request = require('request');
-var apiUrl = 'http://voiceboxpdx.com/api/v1/';
-var app = express.createServer(function (req, res, next) {
-  if (req.url === '/favicon.ico') {
-    return next();
-  }
-  request('http://voiceboxpdx.com/api/v1/songs/stats.json', function (error, response, body) {
-    if (error) {
-      return next(error);
-    }
-    try {
-      var stats = JSON.parse(body);
-    } catch (error) {
-      return next(error);
-    }
-    var song = findSong(stats.top_songs);
-    var room_code = req.url.replace('/','');
-     console.log(room_code);
-    request.post({
-      url: 'http://voiceboxpdx.com/api/v1/queue.json', 
-      form: {
-        room_code: room_code,
-        song_id: song.id
-      }
-    }, function (error, response, body) {
-      if (error) {
-        return next(error);
-      }
-      console.log(body);
-      return res.json(song);
-    });
-  });
-});
+var songs = require('./songs');
 
 function findSong (songs) {
   var song = songs[~~(Math.random() * songs.length)];
@@ -40,12 +7,10 @@ function findSong (songs) {
   }
   return song;
 }
+
 if (!module.parent) {
-  app.listen(11750);
   console.log(11750);
 }
-
-
 
 module.exports = {
   songs: songs
